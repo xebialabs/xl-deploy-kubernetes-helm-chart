8b813ef10ac1ef7a1e56a5abed5810a8375b915f
bnechyporenko <bogdannechiporenko@gmail.com>
2021-11-12 13:30:20 +0100

    updated build_operator.sh

--- build_operator.sh
+++ build_operator.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+containerOrganization="xldevdocker"
+
 if [[ $# -eq 0 ]] ; then
     printf "\e[31mProvide in a first parameter a version (SemVer compatible) to release !\e[m\n"
     echo "For example:"
     printf "\e[1;32m./build_operator.sh 0.0.1\e[0m"
     exit 1
+fi
+
+if [[ $# -eq 2 ]] ; then
+  containerOrganization=$2
 fi
 
 mkdir xld
@@ -21,6 +27,6 @@ rm -rf xl-deploy-kubernetes-helm-chart
 mv digitalai-deploy-*.tgz xld.tgz
 operator-sdk init --domain digital.ai --plugins=helm
 operator-sdk create api --group=xld --version=v1alpha1 --helm-chart=xld.tgz
-export OPERATOR_IMG="docker.io/xldevdocker/deploy-operator:$1"
+export OPERATOR_IMG="docker.io/$containerOrganization/deploy-operator:$1"
 make docker-build docker-push IMG=$OPERATOR_IMG
 rm -rf xld
