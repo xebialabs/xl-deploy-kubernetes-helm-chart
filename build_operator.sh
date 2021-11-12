b63dc515eab30b8892171110bd394df7a0e6c8ed
bnechyporenko <bogdannechiporenko@gmail.com>
2021-11-12 13:01:58 +0100

    Added a script to build an operator from a Helm chart

---
+++ build_operator.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]] ; then
+    printf "\e[31mProvide in a first parameter a version (SemVer compatible) to release !\e[m\n"
+    echo "For example:"
+    printf "\e[1;32m./build_operator.sh 0.0.1\e[0m"
+    exit 1
+fi
+
+mkdir xld
+cd xld
+git clone git@github.com:xebialabs/xl-deploy-kubernetes-helm-chart.git
+cd xl-deploy-kubernetes-helm-chart
+rm -f values-haproxy.yaml
+mv values-nginx.yaml values.yaml
+helm dependency update .
+rm -f Chart.lock
+cd ..
+helm package xl-deploy-kubernetes-helm-chart
+rm -rf xl-deploy-kubernetes-helm-chart
+mv digitalai-deploy-10.1.tgz xld.tgz
+operator-sdk init --domain digital.ai --plugins=helm
+operator-sdk create api --group=xld --version=v1alpha1 --helm-chart=xld.tgz
+export OPERATOR_IMG="docker.io/xldevdocker/deploy-operator:$1"
+make docker-build docker-push IMG=$OPERATOR_IMG
+
