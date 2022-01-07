ca94ebdf37391b7f66f9abc5aa7067ee6eda0fbb
bnechyporenko <bogdannechiporenko@gmail.com>
2022-01-07 14:07:14 +0100

    oidc

--- build_operator.sh
+++ build_operator.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 containerOrganization="xldevdocker"
+upgrade="false"
 
 if [[ $# -eq 0 ]] ; then
     printf "\e[31mProvide in a first parameter a version (SemVer compatible) to release !\e[m\n"
@@ -9,11 +10,19 @@ if [[ $# -eq 0 ]] ; then
     exit 1
 fi
 
-if [[ $# -eq 2 ]] ; then
+if [[ $# > 1 ]] ; then
   containerOrganization=$2
 fi
 
-mkdir xld
+export OPERATOR_IMG="docker.io/$containerOrganization/deploy-operator:$1"
+
+if [[ $# > 2 ]] ; then
+  if [[ "$3" == "true" ]] ; then
+    upgrade="true"
+fi
+fi
+
+#mkdir xld
 cd xld
 git clone git@github.com:xebialabs/xl-deploy-kubernetes-helm-chart.git
 cd xl-deploy-kubernetes-helm-chart
@@ -27,7 +36,39 @@ rm -rf xl-deploy-kubernetes-helm-chart
 mv digitalai-deploy-*.tgz xld.tgz
 operator-sdk init --domain digital.ai --plugins=helm
 operator-sdk create api --group=xld --version=v1alpha1 --helm-chart=xld.tgz
-export OPERATOR_IMG="docker.io/$containerOrganization/deploy-operator:$1"
 make docker-build docker-push IMG=$OPERATOR_IMG
-cd ..
-rm -rf xld
+
+export IMG=$OPERATOR_IMG
+echo "IMG=$OPERATOR_IMG"
+
+export USERNAME=$containerOrganization
+echo "USERNAME=$containerOrganization"
+
+export VERSION=$1
+echo "VERSION=$1"
+
+export BUNDLE_IMG=docker.io/$USERNAME/deploy-operator-bundle:$VERSION
+
+echo "BUNDLE_IMG=docker.io/$USERNAME/deploy-operator-bundle:$VERSION"
+
+operator-sdk olm install
+
+echo "make bundle"
+make bundle
+
+echo "make bundle-build bundle-push"
+make bundle-build bundle-push
+
+echo "operator-sdk bundle validate $BUNDLE_IMG"
+operator-sdk bundle validate $BUNDLE_IMG
+
+if [[ "$upgrade" == "true" ]] ; then
+  echo "operator-sdk run bundle-upgrade $BUNDLE_IMG"
+  operator-sdk run bundle-upgrade $BUNDLE_IMG --timeout 10m0s
+else
+  echo "operator-sdk run bundle $BUNDLE_IMG"
+  operator-sdk run bundle $BUNDLE_IMG --timeout 10m0s
+fi
+
+#cd ..
+#rm -rf xld
