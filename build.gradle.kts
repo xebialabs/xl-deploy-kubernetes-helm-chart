24770e46fb2e1c9d6fe5ed8d433bc149ebb6acb4
Vedran Pugar <vpugar@digital.ai>
2022-02-15 11:33:09 +0100

    ENG-9011 fix maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ plugins {
 
 apply(plugin = "ai.digital.gradle-commit")
 
-group = "ai.digital.deploy.operator"
+group = "ai.digital.deploy.helm"
 project.defaultTasks = listOf("build")
 
 val dockerHubRepository = System.getenv()["DOCKER_HUB_REPOSITORY"] ?: "xebialabsunsupported"
@@ -74,6 +74,10 @@ java {
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+tasks.withType<AbstractPublishToMaven> {
+    dependsOn("buildHelmPackage")
 }
 
 tasks {
@@ -248,15 +252,14 @@ tasks {
 }
 
 publishing {
-    // TODO
-//    publications {
-//        register("operator-archive-$provider", MavenPublication::class) {
-//            artifact(tasks[toOperatorArchiveTaskName(provider)]) {
-//                artifactId = "deploy-operator-$provider"
-//                version = releasedVersion
-//            }
-//        }
-//    }
+    publications {
+        register("digitalai-deploy", MavenPublication::class) {
+            artifact("${buildDir}/xld/xld.tgz") {
+                artifactId = "digitalai-deploy"
+                version = releasedVersion
+            }
+        }
+    }
 
     repositories {
         maven {
