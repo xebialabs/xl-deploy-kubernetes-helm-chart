999921bd000308351b42cf043f64ce365e277743
Vedran Pugar <vpugar@digital.ai>
2022-02-16 08:55:58 +0100

    ENG-9011 fix file

--- kill-hanging-crd-deletion.sh
+++ kill-hanging-crd-deletion.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-oc patch digitalaireleaseocps.xlrocp.digital.ai/dai-ocp-xlr -p '{"metadata":{"finalizers":[]}}' --type=merge
+kubectl patch digitalaireleaseocps.xlrocp.digital.ai/dai-ocp-xlr -p '{"metadata":{"finalizers":[]}}' --type=merge
