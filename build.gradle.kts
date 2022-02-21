b6dab64ab54ae1ee2de23b834a1d1f3144ddba3c
Vedran Pugar <vpugar@digital.ai>
2022-02-21 16:33:52 +0100

    ENG-9011 Add checkDependencyVersions task

--- build.gradle.kts
+++ build.gradle.kts
@@ -249,6 +249,10 @@ tasks {
             logger.lifecycle("Publish to DockerHub $imageUrl finished")
         }
     }
+
+    register("checkDependencyVersions") {
+        // a placeholder to unify with release in jenkins-job
+    }
 }
 
 publishing {
