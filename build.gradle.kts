c086acbe5016a976ccc26388107ff28a300d7f88
Vedran Pugar <72854396+vpugar-digital@users.noreply.github.com>
2022-03-16 04:13:37 +0100

    ENG-8955 bump release version (#105)
    * ENG-8955 bump release version
    
    * ENG-8955 bump release version

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ group = "ai.digital.deploy.helm"
 project.defaultTasks = listOf("build")
 
 val dockerHubRepository = System.getenv()["DOCKER_HUB_REPOSITORY"] ?: "xebialabsunsupported"
-val releasedVersion = System.getenv()["RELEASE_EXPLICIT"] ?: "22.0.0-${
+val releasedVersion = System.getenv()["RELEASE_EXPLICIT"] ?: "22.2.0-${
     LocalDateTime.now().format(DateTimeFormatter.ofPattern("Mdd.Hmm"))
 }"
 project.extra.set("releasedVersion", releasedVersion)
