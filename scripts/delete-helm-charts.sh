c47f8a03baa419b3f54abb956174198f77d37e07
Vedran Pugar <vpugar@digital.ai>
2023-05-27 20:52:54 +0200

    S-92636 Openshift test

--- scripts/delete-helm-charts.sh
+++ scripts/delete-helm-charts.sh
@@ -3,4 +3,4 @@ kubectl delete pvc data-${DAI_RELEASE}-postgresql-0 -n $DAI_NAMESPACE --ignore-n
 kubectl delete pvc data-${DAI_RELEASE}-rabbitmq-0 -n $DAI_NAMESPACE --ignore-not-found=true
 kubectl delete pvc data-${DAI_RELEASE}-rabbitmq-1 -n $DAI_NAMESPACE --ignore-not-found=true
 kubectl delete pvc data-${DAI_RELEASE}-rabbitmq-2 -n $DAI_NAMESPACE --ignore-not-found=true
-kubectl delete pvc data-${DAI_RELEASE}-dai-release -n $DAI_NAMESPACE --ignore-not-found=true
+kubectl delete pvc data-${DAI_RELEASE}-dai-deploy -n $DAI_NAMESPACE --ignore-not-found=true
